TecknoFreak/android_vendor_psx

Add MotoX dalivik patch

products/common.mk
@@ -54,5 +54,10 @@ TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
+# Motox dalvik patch
+ifneq ($(filter psx_mako,$(TARGET_PRODUCT)),)
+$(call inherit-product, vendor/psx/products/motoxdalvikpatch.mk)
+endif
+
 # Inherit kitkat audio package.
 $(call inherit-product, vendor/psx/products/kitkataudio.mk)

products/motoxdalvikpatch.mk
@@ -1,0 +1,16 @@
+# Motox dalvik patch
+
+LOCAL_PATH:= vendor/psx/proprietary/system
+
+# Bin
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/bin/dalvikvm:system/bin/dalvikvm \
+        $(LOCAL_PATH)/bin/dexopt:system/bin/dexopt
+
+# Lib
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/lib/libc.so:system/lib/libc.so \
+        $(LOCAL_PATH)/lib/libcutils.so:system/lib/libcutils.so \
+        $(LOCAL_PATH)/lib/libdvm.so:system/lib/libdvm.so \
+        $(LOCAL_PATH)/lib/libm.so:system/lib/libm.so \
+        $(LOCAL_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so

snak3ater/android_vendor_rascarlo

Conflicts:
	products/common.mk

products/common.mk
@@ -39,3 +39,8 @@ PRODUCT_COPY_FILES += \
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Motox dalvik patch
+    ifneq ($(filter rascarlo_mako,$(TARGET_PRODUCT)),)
+    $(call inherit-product, vendor/rascarlo/products/motoxdalvikpatch.mk)
+endif

products/motoxdalvikpatch.mk
@@ -1,0 +1,16 @@
+# Motox dalvik patch
+
+LOCAL_PATH:= vendor/rascarlo/proprietary/system
+
+# Bin
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/bin/dalvikvm:system/bin/dalvikvm \
+        $(LOCAL_PATH)/bin/dexopt:system/bin/dexopt
+
+# Lib
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/lib/libc.so:system/lib/libc.so \
+        $(LOCAL_PATH)/lib/libcutils.so:system/lib/libcutils.so \
+        $(LOCAL_PATH)/lib/libdvm.so:system/lib/libdvm.so \
+        $(LOCAL_PATH)/lib/libm.so:system/lib/libm.so \
+        $(LOCAL_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so