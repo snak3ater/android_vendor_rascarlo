4c1e5f0423ca843d6377598a8614bbdda06d6e96
snak3ater <saeedyasir91@gmail.com>
2014-03-21 16:02:32 +0500

    Drop corwin42_hammerhead

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,1 @@
-add_lunch_combo rascarlo_emulator-eng
-add_lunch_combo rascarlo_grouper-eng
-add_lunch_combo rascarlo_hammerhead-user
-add_lunch_combo rascarlo_mako-user
-add_lunch_combo rascarlo_manta-user
 add_lunch_combo corwin42_flo-user
