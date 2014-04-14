1985d20be5d51949a0de89b787316d795914ec94
snak3ater <saeedyasir91@gmail.com>
2014-04-14 15:21:39 +0500

    Back to stock Toolchain
    Revert "Enable Graphite & krait optimizations for Mako"
    
    This reverts commit 6274756ad492a292b1e719613964d440183eabc2.

--- products/rascarlo_mako.mk
+++ products/rascarlo_mako.mk
@@ -15,33 +15,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.4.2/KOT49H/937116:user/release-keys \
     PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
 
-# Disable secure discard flag
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-
-# Enable graphite optimizations
-GLOBAL_ENABLE_GRAPHITE := true
-
-# Krait optimizations
-TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
-TARGET_USE_KRAIT_PLD_SET := true
-TARGET_KRAIT_BIONIC_PLDOFFS := 10
-TARGET_KRAIT_BIONIC_PLDTHRESH := 10
-TARGET_KRAIT_BIONIC_BBTHRESH := 64
-TARGET_KRAIT_BIONIC_PLDSIZE := 64
-
-# Target arch variant cpu
-TARGET_ARCH_VARIANT_CPU := cortex-a15
-
-# Arch arm high optimization
-ARCH_ARM_HIGH_OPTIMIZATION := true
-
-# Flags for krait cpu
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_ARCH_VARIANT_FPU := neon-vfpv4
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a15
-
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
 
